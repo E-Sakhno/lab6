5b622986791ea2a3648df8fa4a043ac9740b737e
E-Sakhno <miss.saxno@yandex.ru>
2020-12-24 21:29:32 +0300

    Решение задачи по пункту 8

---
+++ task1.py
@@ -0,0 +1,13 @@
+if __name__ == '__main__':
+    school = {'1а': 25, '1б': 24, '2б': 27, '6а': 23, '7а': 25, '7в': 31, '11а': 29, '11б': 28}
+    print(school)
+    school['1а'] = 30
+    school['10в'] = 25
+    del school['7в']
+    total = 0
+    for group in school:
+        total += school[group]
+    print(school)
+    print(total)
+
+
