24bd5f580b91ad44bd59322345d1ebb9d5353d3c
E-Sakhno <miss.saxno@yandex.ru>
2020-12-24 21:39:03 +0300

    Решение задачи по пункту 10

---
+++ task2.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    school = {25: '7а', 24: '1б', 27: '2б', 23: '6а', 31: '7в', 29: '11а', 28: '11б'}
+    print(school)
+    school_reverse = {}
+    for key, value in school.items():
+        school_reverse[value] = key
+    print(school_reverse)
